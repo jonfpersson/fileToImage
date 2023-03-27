42083465135ab074baaab805738c3ee5a678d125
jonfpersson <jonfpersson@gmail.com>
2023-03-27 21:23:59 +0200

    Encode size, rows and cols in pixel data

--- fileToImages.cpp
+++ fileToImages.cpp
@@ -3,16 +3,24 @@
 #include <stdlib.h>
 using namespace cv;
 
-void create_images(int frames, int rows, int cols, int size, uint8_t* index, Mat** images){
+void create_images(int frames, int rows, int cols, long size, uint8_t* index, Mat** images){
     long loopIndex = 0;
 
     for (int i = 0; i < frames; i++) {
         images[i] = new cv::Mat(rows, cols, CV_8UC3); // create a new cv::Mat object
     }
+    //Split 64 bit number into 3*8bit number to encode size in pixels
+    uint64_t number = size;
+    uint8_t part1 = (number >> 16) & 0xFF;  // Most significant 8 bits
+    uint8_t part2 = (number >> 8) & 0xFF;   // Middle 8 bits
+    uint8_t part3 = number & 0xFF;
+    (images[0])->at<Vec3b>(Point(0,0)) = Vec3b (part1, part2, part3);
+    (images[0])->at<Vec3b>(Point(0,1)) = Vec3b (rows, cols, cols);
 
+    printf("%i   %i    %i\n", part1, part2, part3);
     for(int i = 0; i < frames; i++){
-        for(int y = 0; y < rows; y++){
-            for(int x = 0; x < cols; x++){
+        for(int row = 0; row < rows; row++){
+            for(int col = 2; col < cols; col++){
                 //Check if we're at the last byte
                 if(loopIndex == size){
                     return;
@@ -29,20 +37,24 @@ void create_images(int frames, int rows, int cols, int size, uint8_t* index, Mat
 
                 loopIndex += 3;
                 //Set the colors (bytes)
-                (images[i])->at<Vec3b>(Point(x,y)) = Vec3b (byteOne, byteTwo, byteThree);;
+                (images[i])->at<Vec3b>(Point(row, col)) = Vec3b (byteOne, byteTwo, byteThree);;
             }
         }
     }
 }
 
 int main() {
-    FILE *frog = fopen("frogg.webp","rb");
+    FILE *frog = fopen("frog3.jpg","rb");
     
     //Get file size
     fseek(frog, 0, SEEK_END);
     // Determine file size
-    long size = ftell(frog) + 1;
+    long size = ftell(frog);
     fseek(frog, 0, SEEK_SET);
+    if(size % 3 != 0){
+        fclose(frog);
+        return 1;
+    }
 
     //Allocate memory for picture
     uint8_t* buffer = (uint8_t*) malloc(size * sizeof(uint8_t));
@@ -50,8 +62,8 @@ int main() {
     uint8_t* index = buffer;
 
     printf("size %lu \n", size);
-    int cols = 50;
-    int rows = 50;
+    int cols = 300;
+    int rows = 300;
     int frames = size/(3*rows*cols);
     int leftOverPixels = size - frames * (cols * rows);
     //We do this to account for the last non-full frame
@@ -62,20 +74,17 @@ int main() {
 
     create_images(frames, rows, cols, size, index, images);
 
-end:
-    printf("frames %i\n", frames);
-
     // free the memory
     for (int i = 0; i < frames; i++) {
-        imwrite("pics/" + std::to_string(i) + ".jpg",*images[i]);
+        imwrite("pics/" + std::to_string(i) + ".png",*images[i]);
         // delete the cv::Mat object
         delete images[i];
     }
     // delete the array of pointers
     delete[] images;
 
-    fclose(frog);
     free(buffer);
+    fclose(frog);
 
     return 0;
 }
